fa3ca64752264bc5795d64f7b7b545f454d70670
Raven <reevan0613@gmail.com>
2022-08-19 12:07:57 +0800

    add middleware: auth.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const { authenticator } = require('../middleware/auth')
 const router = express.Router();
 
 const home = require('./modules/home');
 const restaurant = require('./modules/restaurant')
 const user = require('./modules/user');
 
-router.use('/' , home);
-router.use('/restaurants' , restaurant);
+
+router.use('/restaurants' , authenticator , restaurant);
 router.use('/users' , user)
+router.use('/' , authenticator , home);
 
 
 module.exports = router;
