daa8f2ecb35864e9bbccaa4a44cb4deab8cd3684
Raven <reevan0613@gmail.com>
2022-08-23 10:28:17 +0800

    add sort routes

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,6 +4,7 @@ const Restaurant = require('../../models/Restaurants');
 const router = express.Router();
 
 
+
  //搜尋結果頁面
 router.get('/search' , (req , res) => {
 
@@ -12,7 +13,7 @@ router.get('/search' , (req , res) => {
   let currentKeyword = req.query.keyword.replace(/\s*/g, "");
 
   let filteredRestaurant = [];
-  
+
   function getResultAmount(){
     return filteredRestaurant.length?
     `- 搜尋餐廳或分類含「${currentKeyword}」 之結果共 ${filteredRestaurant.length} 筆 -` :
@@ -32,10 +33,57 @@ router.get('/search' , (req , res) => {
             .catch(error => console.log(error))
 });
 
+//sort結果頁面-依名字排序
+router.get(('/sort_asc') , (req , res) => {
+  const userId = req.user._id;
+
+  return Restaurant
+  .find({ userId })
+  .lean()
+  .sort({ name : 'asc'})
+  .then(restaurants => res.render('index' , {restaurantList : restaurants , asc_selected : 'selected'}))
+  .catch(error => console.log(error))
+});
+//sort結果頁面-依名字排序
+router.get(('/sort_desc') , (req , res) => {
+  const userId = req.user._id;
+  selectedsort = 'Z-A';
+  return Restaurant
+  .find({ userId })
+  .lean()
+  .sort({ name : 'desc'})
+  .then(restaurants => res.render('index' , {restaurantList : restaurants , desc_selected : 'selected'}))
+  .catch(error => console.log(error))
+});
+//sort結果頁面-依類別排序
+router.get(('/sort_category') , (req , res) => {
+  const userId = req.user._id;
+
+  return Restaurant
+  .find({ userId })
+  .lean()
+  .sort({ category : 'asc'})
+  .then(restaurants => res.render('index' , {restaurantList : restaurants , category_selected : 'selected'}))
+  .catch(error => console.log(error))
+});
+//sort結果頁面-依地區排序
+router.get(('/sort_location') , (req , res) => {
+  const userId = req.user._id;
+
+  return Restaurant
+  .find({ userId })
+  .lean()
+  .sort({ location : 'asc'})
+  .then(restaurants => res.render('index' , {restaurantList : restaurants , location_selected : 'selected'}))
+  .catch(error => console.log(error))
+});
+
 
 
 //首頁
 router.get('/' , (req , res) => {
+
+
   const userId = req.user._id;
 
   Restaurant.find({ userId })
